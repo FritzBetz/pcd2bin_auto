b35003b0514bd2fbb5b4556c7d824f8110d77828
Friedrich Betz <friedrich.betz@alumni.fh-aachen.de>
2024-12-05 20:02:14 +0100

    working version

--- pcd2bin.py
+++ pcd2bin.py
@@ -13,7 +13,7 @@ import errno
 import numpy as np
 import os
 import argparse
-import pypcd
+from pypcd_custom import pypcd
 import csv
 from tqdm import tqdm
 
@@ -22,92 +22,76 @@ def main():
     parser = argparse.ArgumentParser(description="Convert .pcd to .bin")
     parser.add_argument(
         "--pcd_path",
-        help=".pcd file path.",
+        help="Folder path containing .pcd files.",
         type=str,
         default="/home/user/lidar_pcd"
     )
-    parser.add_argument(
-        "--bin_path",
-        help=".bin file path.",
-        type=str,
-        default="/home/user/lidar_bin"
-    )
-    parser.add_argument(
-        "--file_name",
-        help="File name.",
-        type=str,
-        default="file_name"
-    )
+
     args = parser.parse_args()
 
-    ## Find all pcd files
+    ## Get the base folder name for the bin files
+    pcd_folder_name = os.path.basename(os.path.normpath(args.pcd_path))
+    
+    ## Make bin folder under the current directory
+    bin_path = os.path.join("./", pcd_folder_name)
+    try:
+        if not os.path.isdir(bin_path):
+            os.makedirs(bin_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print("Failed to create directory!!!!!")
+            raise
+
+    ## Find all pcd files in the provided folder
     pcd_files = []
     for (path, dir, files) in os.walk(args.pcd_path):
         for filename in files:
-            # print(filename)
             ext = os.path.splitext(filename)[-1]
             if ext == '.pcd':
-                pcd_files.append(path + "/" + filename)
+                pcd_files.append(os.path.join(path, filename))
 
     ## Sort pcd files by file name
-    pcd_files.sort()   
-    print("Finish to load point clouds!")
-
-    ## Make bin_path directory
-    try:
-        if not (os.path.isdir(args.bin_path)):
-            os.makedirs(os.path.join(args.bin_path))
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            print ("Failed to create directory!!!!!")
-            raise
+    pcd_files.sort()
+    print("Finished loading point clouds!")
 
     ## Generate csv meta file
-    csv_file_path = os.path.join(args.bin_path, "meta.csv")
-    csv_file = open(csv_file_path, "w")
-    meta_file = csv.writer(
-        csv_file, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
-    )
-    ## Write csv meta file header
-    meta_file.writerow(
-        [
-            "pcd file name",
-            "bin file name",
-        ]
-    )
-    print("Finish to generate csv meta file")
+    csv_file_path = os.path.join(bin_path, "meta.csv")
+    with open(csv_file_path, "w", newline="") as csv_file:
+        meta_file = csv.writer(csv_file, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
 
-    ## Converting Process
-    print("Converting Start!")
-    seq = 0
-    for pcd_file in tqdm(pcd_files):
-        ## Get pcd file
-        pc = pypcd.PointCloud.from_path(pcd_file)
+        ## Write csv meta file header
+        meta_file.writerow(["pcd file name", "bin file name"])
+        print("Finished generating csv meta file")
 
-        ## Generate bin file name
-        bin_file_name = "{}_{:05d}.bin".format(args.file_name, seq)
-        bin_file_path = os.path.join(args.bin_path, bin_file_name)
-        
-        ## Get data from pcd (x, y, z, intensity, ring, time)
-        np_x = (np.array(pc.pc_data['x'], dtype=np.float32)).astype(np.float32)
-        np_y = (np.array(pc.pc_data['y'], dtype=np.float32)).astype(np.float32)
-        np_z = (np.array(pc.pc_data['z'], dtype=np.float32)).astype(np.float32)
-        np_i = (np.array(pc.pc_data['intensity'], dtype=np.float32)).astype(np.float32)/256
-        # np_r = (np.array(pc.pc_data['ring'], dtype=np.float32)).astype(np.float32)
-        # np_t = (np.array(pc.pc_data['time'], dtype=np.float32)).astype(np.float32)
+        ## Converting Process
+        print("Converting Start!")
+        seq = 0
+        for pcd_file in (pcd_files):
+            ## Get pcd file
+            pc = pypcd.PointCloud.from_path(pcd_file)
 
-        ## Stack all data    
-        points_32 = np.transpose(np.vstack((np_x, np_y, np_z, np_i)))
+            ## Generate bin file name
+            bin_file_name = "{}_{:05d}.bin".format(os.path.splitext(os.path.basename(pcd_file))[0], seq)
+            bin_file_path = os.path.join(bin_path, bin_file_name)
 
-        ## Save bin file                                    
-        points_32.tofile(bin_file_path)
+            ## Get data from pcd (x, y, z, intensity)
+            np_x = np.array(pc.pc_data['x'], dtype=np.float32)
+            np_y = np.array(pc.pc_data['y'], dtype=np.float32)
+            np_z = np.array(pc.pc_data['z'], dtype=np.float32)
+            np_i = np.array(pc.pc_data['intensity'], dtype=np.float32) / 256.0
 
-        ## Write csv meta file
-        meta_file.writerow(
-            [os.path.split(pcd_file)[-1], bin_file_name]
-        )
+            ## Stack all data
+            points_32 = np.transpose(np.vstack((np_x, np_y, np_z, np_i)))
 
-        seq = seq + 1
-    
+            ## Save bin file
+            points_32.tofile(bin_file_path)
+
+            ## Write csv meta file
+            meta_file.writerow([os.path.split(pcd_file)[-1], bin_file_name])
+
+            seq += 1
+
+    print("Conversion Complete!")
+
 if __name__ == "__main__":
     main()
